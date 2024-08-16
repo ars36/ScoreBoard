9a359850a647302666dafdf2cb44436a5f58407f
taileduc0404 <taileduc0404@gmail.com>
2024-08-16 16:58:33 +0700

    leductai

--- ScoreBoard/Controllers/HomeController.cs
+++ ScoreBoard/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 
 namespace ScoreBoard.Controllers
 {
+    // Lê Đức Tài
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
